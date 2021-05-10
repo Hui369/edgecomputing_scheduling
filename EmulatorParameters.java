c3d26c395a8cfe80d145b0d1186f2caaa3581325
Hui369 <52521232+Hui369@users.noreply.github.com>
2021-05-10 14:09:13 +0800

    Create EmulatorParameters.java

---
+++ EmulatorParameters.java
@@ -0,0 +1,12 @@
+package ntust.huiting.edgecomputing.v2;
+
+/**
+ * 環境參數
+ * @author hui
+ *
+ */
+public interface EmulatorParameters {
+	final public int queue_number = 1; // queue數量
+	final public int constant_deadline_time = 1000; // 截止時間
+	final public int service_number = 3; // 應用類型數量
+}
